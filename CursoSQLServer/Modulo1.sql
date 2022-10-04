b356e6033a1e4974356494141654bea7a05bed51
Yael Chavez <you400@gmail.com>
2022-10-03 19:40:19 -0500

    numeros enteros

--- CursoSQLServer/Modulo1.sql
+++ CursoSQLServer/Modulo1.sql
@@ -8,3 +8,21 @@ select 1/1 as [Division], 1+1 as [Suma]
 -- alias y multiples campos
 
 --select 1/0
+
+
+--- Modulo Variables y tipos de datos numericos
+
+declare @myVar as int = 3000000000
+declare @MyVar2 as tinyInt = 255
+
+--set @myVar =  @myVar + @myVar2
+
+select @MyVar2 as Resultado
+
+	--- tipos de datos enteros
+
+	--- int hasta 2,000,000,000
+	--- tinyInt 255
+	--- smallint hasta 32768
+	--- bigint el mas grande
+
