ad5aebdbf674343a323f0b15983f08ed3f78f03f
Yael Chavez <you400@gmail.com>
2022-10-03 19:20:03 -0500

    Modulo select y alias

---
+++ CursoSQLServer/Modulo1.sql
@@ -0,0 +1,10 @@
+--- Uso del select y alias
+select 1
+
+select 1+1 as [Sum] --Alias
+
+select 1/1 as [Division], 1+1 as [Suma]
+, 1*1 as [Multiplicacion] 
+-- alias y multiples campos
+
+--select 1/0
