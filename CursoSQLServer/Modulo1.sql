8344f87bc9607c947b67c8bcacfcbbb6caca342b
Yael Chavez <you400@gmail.com>
2022-10-03 19:51:45 -0500

    decimales

--- CursoSQLServer/Modulo1.sql
+++ CursoSQLServer/Modulo1.sql
@@ -19,10 +19,27 @@ declare @MyVar2 as tinyInt = 255
 
 select @MyVar2 as Resultado
 
-	--- tipos de datos enteros
+	--- tipos numericos enteros
 
 	--- int hasta 2,000,000,000
 	--- tinyInt 255
 	--- smallint hasta 32768
 	--- bigint el mas grande
+
+   -- tipos numericos no enteros
+
+   declare @myVarDecimal as decimal(18,4)
+
+   set @myVarDecimal = 1234.679904564
+
+   select @myVarDecimal
+
+  --declare @MyVarmoney as money;
+  declare @MyVarmoney as smallmoney;
+
+   set @MyVarmoney = 123456.1234567
+
+   select @MyVarmoney
+
+
 
