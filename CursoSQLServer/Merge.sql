df2d44d1a4c3106b7fec3329d977484c23a46af3
Yael Chavez <you400@gmail.com>
2022-10-06 18:14:40 -0500

    Structured types

--- CursoSQLServer/Merge.sql
+++ CursoSQLServer/Merge.sql
@@ -4,12 +4,13 @@ update
 delete
 Merge*/
 
---select * into tblTransactionNew  from  tblTransaction
-
+SELECT *
+INTO tblTransactionNew
+FROM tblTransaction;
 MERGE INTO tbltransaction AS t --- tabla a hacerle merge
 USING tblTransactionNew AS N --- la tabla de la que traeremos la informacion
 ON t.employeenumber = N.employeenumber
-   AND t.dateoftransaction = N.DateOfTransaction 
+   AND t.dateoftransaction = N.DateOfTransaction
    AND t.amount = n.amount--estableciendo el criterio de combinacion
     WHEN MATCHED
     THEN UPDATE SET 
@@ -24,5 +25,73 @@ ON t.employeenumber = N.employeenumber
  n.dateoftransaction, 
  n.employeenumber
 )
-output deleted.*, inserted.*; -- output nos regresa los insertado y lo modificado
+OUTPUT deleted.*, 
+       inserted.*; -- output nos regresa los insertado y lo modificado
+
+SELECT *
+INTO #tmpTransaction
+FROM tblTransaction; --trablas fisicas temporales
+
+SELECT *
+INTO ##tmpTransaction
+FROM tblTransaction; --trablas fisicas temporales globales
+
+SELECT *
+FROM ##tmpTransaction;
+DECLARE @tmpTransaction AS TABLE --tablas temporales tipo variable
+(Amount            SMALLMONEY, 
+ DateOfTransaction SMALLDATETIME, 
+ EmployeeNumber    INT
+);
+INSERT INTO @tmpTransaction
+       SELECT *
+       FROM tblTransaction;
+SELECT *
+FROM @tmpTransaction
+WHERE EmployeeNumber = 123;
+GO
+CREATE TYPE tblTransactionType AS TABLE
+(Amount            SMALLMONEY, 
+ DateOfTransaction SMALLDATETIME, 
+ EmployeeNumber    INT
+);
+GO
+DECLARE @transacciones AS TBLTRANSACTIONTYPE;
+INSERT INTO @transacciones
+VALUES
+(2001, 
+ GETDATE(), 
+ 1
+),
+(2002, 
+ GETDATE(), 
+ 2
+),
+(2003, 
+ GETDATE(), 
+ 3
+),
+(2004, 
+ GETDATE(), 
+ 4
+),
+(2005, 
+ GETDATE(), 
+ 5
+);
+
+--select * from @transacciones
+--mandamos llamar nuestro SP
+
+EXEC MergeTransactions 
+     @transacciones;
+
+--- ponerlo en un stored procedure
+GO
+CREATE PROC MergeTransactions @transacciones AS TBLTRANSACTIONTYPE READONLY
+AS
+     INSERT INTO tblTransaction
+            SELECT *
+            FROM @transacciones
+
 
