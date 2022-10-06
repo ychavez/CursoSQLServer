89ccd0016eef0405b16e5f2e805bd88ccff31459
Yael Chavez <you400@gmail.com>
2022-10-05 20:44:05 -0500

    funciones

--- CursoSQLServer/Vistas.sql
+++ CursoSQLServer/Vistas.sql
@@ -18,8 +18,7 @@ FROM miPrimeraVista;
 
 -- crear una vista que nos muestre las transacciones totales por empleado necesitamos
 -- nombre de empleado- numero de empleado y monto sin nullos
-go
-
+GO
 CREATE VIEW TransaccionesPorEmpleado
 AS
      SELECT e.EmployeeFirstName, 
@@ -29,6 +28,62 @@ AS
           JOIN tblTransaction t ON t.EmployeeNumber = e.EmployeeNumber
      GROUP BY e.EmployeeFirstName, 
               e.EmployeeNumber;
-Go
+GO
+SELECT *
+FROM TransaccionesPorEmpleado;
 
-select * from TransaccionesPorEmpleado
+--- el sistema nos pide las transacciones por cierto usuario
+--123
+SELECT *
+FROM TransaccionesPorEmpleado
+WHERE EmployeeNumber = 123;
+
+--- funciones tipo tabla
+
+GO
+CREATE FUNCTION fnTransaccionesPorEmpleado
+(@employeeNumber INT
+)
+RETURNS TABLE
+AS
+     RETURN
+     SELECT e.EmployeeFirstName, 
+            e.EmployeeNumber, 
+            SUM(t.amount) [Total]
+     FROM tblEmployee e
+          JOIN tblTransaction t ON t.EmployeeNumber = e.EmployeeNumber
+     WHERE e.EmployeeNumber = @employeeNumber
+     GROUP BY e.EmployeeFirstName, 
+              e.EmployeeNumber;
+SELECT *
+FROM fnTransaccionesPorEmpleado(125);
+GO
+
+
+
+
+
+CREATE FUNCTION fnTransaccionesPorEmpleadoNoInfer
+(@employeeNumber INT
+)
+RETURNS @empleadosTransacciones TABLE
+(NombreEmpleado VARCHAR(100), 
+ NumeroEmpleado INT, 
+ Transacciones  MONEY
+)
+AS
+     BEGIN
+         INSERT INTO @empleadosTransacciones
+                SELECT e.EmployeeFirstName, 
+                       e.EmployeeNumber, 
+                       SUM(t.amount) [Total]
+                FROM tblEmployee e
+                     JOIN tblTransaction t ON t.EmployeeNumber = e.EmployeeNumber
+                WHERE e.EmployeeNumber = @employeeNumber
+                GROUP BY e.EmployeeFirstName, 
+                         e.EmployeeNumber;
+         RETURN;
+     END;
+
+SELECT *
+FROM fnTransaccionesPorEmpleadoNoInfer(123);
